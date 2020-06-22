ea270175092a6c62ea37827f4619d12f69c2552a
seongbinyoon <yoonseongbin4925@gmail.com>
2020-06-22 18:55:42 +0900

    button구현

--- app/src/main/java/com/example/board4/ViewActivity.java
+++ app/src/main/java/com/example/board4/ViewActivity.java
@@ -2,11 +2,18 @@ package com.example.board4;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class ViewActivity extends AppCompatActivity {
+
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,20 @@ public class ViewActivity extends AppCompatActivity {
         writer.setText(intent.getStringExtra("writer"));
         date.setText(intent.getStringExtra("date"));
         content.setText(intent.getStringExtra("content"));
+
+        //버튼 선언, xml에서 가져오기
+        final Button btn_like = (Button) findViewById(R.id.btn_like);
+
+        btn_like.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                count++;
+                Toast.makeText(getApplicationContext(), count + "명이 좋아요를 눌렀습니다", Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
 
     }
 
